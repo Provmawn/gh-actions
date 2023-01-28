a6cfa1e8e33a29b716fdd4fcd6e3b6606260e516
provmawn <provmawn@gmail.com>
2023-01-28 10:50:49 -0800

    Revert "fail test"
    This reverts commit dd220c501da97f7570380cdd6c2bd9f70651037b.

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
     got := Hello()
-    want := "Hello, World! FAILED"
+    want := "Hello, World!"
     if got != want {
         t.Errorf("got %q, want %q\n", got, want)
     }
